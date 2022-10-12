1aa1087a711b8f21d5efd4c771f8cc48e6a337d0
kky <kky9616tt@gmail.com>
2022-10-13 03:12:55 +0900

    2차 수정

--- public/js/api.js
+++ public/js/api.js
@@ -1,10 +1,14 @@
-// api키
+ // api키
 let api_key = "d516745898f6293a295ed20361b456e5";
 
-
+// 이미지
 let img_url = "https://image.tmdb.org/t/p/w500";
+// 원본 이미지
 let original_img_url = "https://image.tmdb.org/t/p/original";
+// 장르
 let genres_list_http = "https://api.themoviedb.org/3/genre/movie/list?";
+// 정보 가져오기
 let movie_genres_http = "https://api.themoviedb.org/3/discover/movie?";
+// 영화 세부 정보
 let movie_detail_http = "https://api.themoviedb.org/3/movie";
 
