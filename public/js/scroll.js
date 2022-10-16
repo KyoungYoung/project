b175dc2a6eee59962744fcc218b407ca952f1a84
Kyoung Young <kky9616tt@gmail.com>
2022-10-17 01:49:02 +0900

    출연진 슬라이드 생성 및 디자인 수정중

--- public/js/scroll.js
+++ public/js/scroll.js
@@ -1,5 +1,4 @@
-// 슬라이더 생성
-
+// 메인 페이지 영화 목록 슬라이더 생성
 const setupScrolling = () => {
     const container = [...document.querySelectorAll('.movie-container')];
     const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
@@ -19,4 +18,25 @@ const setupScrolling = () => {
     })
 
 }
+
+// 서브 페이지 출연진 슬라이더 생성
+// const castScrolling = () => {
+//     const list = [...document.querySelectorAll('.cast-list')];
+//     const nxtBtn = [...document.querySelectorAll('.nxt-btn')];
+//     const preBtn = [...document.querySelectorAll('.pre-btn')]; 
+
+//     list.cast.forEach((item,j) => {
+//         let containerDimensions = item.getBoundingClientRect();
+//         let containerWidth = containerDimensions.width;
+
+//         nxtBtn[j].addEventListener('click', () => {
+//             item.scrollLeft += containerWidth;
+//         })
+
+//         preBtn[j].addEventListener('click', () => {
+//             item.scrollLeft -= containerWidth;
+//         })
+//     })
+
+// }
 
