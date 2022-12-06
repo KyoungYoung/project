e8b35b2a2bfcc7d57ed19fd2ec715bcfc97f7d19
kky <kky9616tt@gmail.com>
2022-12-06 22:44:34 +0900

    test1

--- capston2-tetris/server.js
+++ capston2-tetris/server.js
@@ -3,6 +3,9 @@ const app = express();
 const port = 8080;
 const bodyParser = require('body-parser');
 app.use(bodyParser.urlencoded({extends : true}));
+
+// ejs 설정
+app.set('views',__dirname + '/views')
 app.set('view engine', 'ejs');
 // mongoDB 연결
 let db;
@@ -44,7 +47,13 @@ app.use(express.static('public/js'));
 app.use(express.static('public/audio_src'));
 
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname + '/Lobby.html')
+    db.collection('post').find().toArray((err,result)=>{
+        result.sort((a,b)=>{ 
+            return b.point - a.point
+        }) 
+        console.log(result);
+        res.render('Lobby.ejs',{posts : result});
+    });
 });
 app.get('/1P-game',(req,res)=>{
     res.sendFile(__dirname + '/1Pindex.html')
@@ -56,17 +65,17 @@ app.get('/write',(req,res)=>{
     res.sendFile(__dirname + '/write.html')
 });
 
-// 전송 받은 데이터 보여주기
-app.get('/list',(req,res)=>{
-    // 모든 데이터 보여주기
-    db.collection('post').find().toArray((err,result)=>{
-        result.sort((a,b)=>{ 
-            return b.point - a.point
-        }) 
-        console.log(result);
-        res.render('list.ejs',{posts : result});
-    });
-}); 
+// // 전송 받은 데이터 보여주기
+// app.get('/list',(req,res)=>{
+//     // 모든 데이터 보여주기
+//     db.collection('post').find().toArray((err,result)=>{
+//         result.sort((a,b)=>{ 
+//             return b.point - a.point
+//         }) 
+//         console.log(result);
+//         res.render('list.ejs',{posts : result});
+//     });
+// }); 
 // 삭제
 app.delete('/delete',(req,res) => {
     console.log(req.body);
